9b666c5c64a0546c7eb002764a21bef1704e6808
jaeeun <jaeeun@com2us.com>
2025-07-08 17:56:40 +0900

    trait 실습

--- run.php
+++ run.php
@@ -1,15 +1,39 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+trait Cacheable {
+    private array $cache = [];
+    
+    public function cacheGet(string $key) {
+        return $this->cache[$key] ?? null;
+    }
+    
+    public function cacheSet(string $key, $value): void {
+        $this->cache[$key] = $value;
+    }
+    
+    public function cacheHas(string $key): bool {
+        return isset($this->cache[$key]);
+    }
+    
+    public function cacheClear(): void {
+        $this->cache = [];
+    }
+}
 
-use App\Models\MemoryProductRepository;
-use App\Models\Product;
-
-$product = new Product();
-$product->id = 1;
-$product->name = "삼겹살";
-
-$repo = MemoryProductRepository::getInstance();
-$repo->save($product);
-
-echo $repo->findById(1)->name; // 삼겹살
+class ProductService {
+    use Cacheable;
+    
+    public function getProduct(int $id) {
+        $cacheKey = "product_{$id}";
+        
+        if ($this->cacheHas($cacheKey)) {
+            return $this->cacheGet($cacheKey);
+        }
+        
+        // 데이터베이스에서 상품 조회 (예시)
+        $product = ['id' => $id, 'name' => "Product {$id}"];
+        
+        $this->cacheSet($cacheKey, $product);
+        return $product;
+    }
+}
